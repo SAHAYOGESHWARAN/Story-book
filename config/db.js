82caae6e58eeb7da6e2edd8276df41be047c6aa9
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-21 11:29:04 +0530

    storybook

--- config/db.js
+++ config/db.js
@@ -5,16 +5,14 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useFindAndModify:false
-})
-console.log(`MongoDB Connected: ${conn.connection.host}`
-    )
+            // Remove useFindAndModify: false
+        })
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`)
     } catch (err) {
         console.error(err)
         process.exit(1)
-        }
-        }
-        
-        module.exports = connectDB;
+    }
+}
 
-
+module.exports = connectDB
