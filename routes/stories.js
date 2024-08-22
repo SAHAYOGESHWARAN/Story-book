31ef60b566f4f0830de54c624742077fa54846c3
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-22 09:44:15 +0530

    storybook

---
+++ routes/stories.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const { ensureAuth } = require('../middleware/auth')
+
+const Story = require('../models/story')
+
+// @desc     show add page
+// @route   GET /stories/add
+router.get('/add', ensureGuest, (req, res) => {
+  res.render('stories/add', {
+    
+  })
+})
+
+
+
+module.exports = router
