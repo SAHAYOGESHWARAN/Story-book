9ffe3fb0bd54f7bcf4d4ae52fd556c70c4d4b65d
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-22 08:43:19 +0530

    storybook

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require('../middleware/auth');
-
-const Story = require('../models/story')
+const Story = require('../models/story');
 
 // @desc Login/landing page
 // @route GET /
@@ -14,20 +13,17 @@ router.get('/', ensureGuest, (req, res) => {
 
 // @desc Dashboard
 // @route GET /dashboard
-router.get('/dashboard', ensureAuth, (req, res) => {
-    try{
-        const stories = await Story.find({user:req.user.id}).lern()
+router.get('/dashboard', ensureAuth, async (req, res) => { // Marked the function as async
+    try {
+        const stories = await Story.find({ user: req.user.id }).lean();
         res.render('dashboard', {
-            name:req.user.name,
-            stories: stories
-    })
-    } catch(err){
-        console.error(err)
-        res.status(500).send('Error')
-        }
-    res.render('dashboard', {
-        name: req.user.firstName,
-    });
+            name: req.user.firstName,
+            stories: stories,
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
 });
 
 // Export the router
